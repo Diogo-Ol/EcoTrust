e5f2a0321e849bcba383943dbbcf6d182af4bdfe
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-09-15 16:45:08 -0300

    Subindo home faltando somente animar o modal

---
+++ Componentes/Carousel/index.js
@@ -0,0 +1,8 @@
+import { CarouselDepoimentos } from "./carouselDepoimentos.js"
+
+const anteriorDepoimentos = '[data-anterior-depoimentos]'
+const proximoDepoimentos = '[data-proximo-depoimentos]'
+const listaProdutosDepoimentos = '[data-lista-depoimentos]'
+const navegacaoDepoimentos = '[data-navegacao-depoimentos]'
+
+new CarouselDepoimentos(anteriorDepoimentos, proximoDepoimentos, listaProdutosDepoimentos,navegacaoDepoimentos)
