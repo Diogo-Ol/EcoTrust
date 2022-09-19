a2418b23bf3cb673ab2144b191efb9da6825fee0
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-09-18 22:16:45 -0300

    Finalizando Sobre

--- Componentes/Carousel/index.js
+++ Componentes/Carousel/index.js
@@ -6,3 +6,20 @@ const listaProdutosDepoimentos = '[data-lista-depoimentos]'
 const navegacaoDepoimentos = '[data-navegacao-depoimentos]'
 
 new CarouselDepoimentos(anteriorDepoimentos, proximoDepoimentos, listaProdutosDepoimentos,navegacaoDepoimentos)
+
+import { CarouselTempo } from "./carouselLinhaTempo.js"
+
+const anteriorTempo = '[data-anterior-tempo]'
+const proximoTempo = '[data-proximo-tempo]'
+const listaProdutosTempo = '[data-lista-tempo]'
+const navegacaoTempo = '[data-navegacao-tempo]'
+
+new CarouselTempo(anteriorTempo, proximoTempo, listaProdutosTempo,navegacaoTempo)
+
+import { CarouselNoticias } from "./carouselNoticias.js"
+
+const anteriorNoticias = '[data-anterior-noticias]'
+const proximoNoticias = '[data-proximo-noticias]'
+const listaProdutosNoticias = '[data-lista-noticias]'
+
+new CarouselNoticias(anteriorNoticias, proximoNoticias, listaProdutosNoticias)
