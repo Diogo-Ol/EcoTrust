61a863ec79dcae4dbb651659bf121918aee33f39
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-09-19 10:46:55 -0300

    Finalizando Funcionalidades e iniciando responsivo das telas

--- Componentes/Carousel/index.js
+++ Componentes/Carousel/index.js
@@ -23,3 +23,11 @@ const proximoNoticias = '[data-proximo-noticias]'
 const listaProdutosNoticias = '[data-lista-noticias]'
 
 new CarouselNoticias(anteriorNoticias, proximoNoticias, listaProdutosNoticias)
+
+import { CarouselBeneficios } from "./carouselBeneficios.js"
+
+const anteriorBeneficios = '[data-anterior-beneficios]'
+const proximoBeneficios = '[data-proximo-beneficios]'
+const listaProdutosBeneficios = '[data-lista-beneficios]'
+
+new CarouselBeneficios(anteriorBeneficios, proximoBeneficios, listaProdutosBeneficios)
